44672514cac72652dd54e935fc68216c1dc80ff0
Christian Hansen <64793a1a@gmail.com>
2020-04-11 12:40:07 +0200

    patch: fixes hopper tick issue

--- src/main/java/com/Acrobot/ChestShop/Listeners/Item/ItemMoveListener.java
+++ src/main/java/com/Acrobot/ChestShop/Listeners/Item/ItemMoveListener.java
@@ -2,10 +2,14 @@ package com.Acrobot.ChestShop.Listeners.Item;
 
 import com.Acrobot.ChestShop.Signs.ChestShopSign;
 import org.bukkit.block.BlockState;
+import org.bukkit.block.Hopper;
+import org.bukkit.entity.minecart.HopperMinecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
 
 /**
  * @author Acrobot
@@ -14,6 +18,10 @@ public class ItemMoveListener implements Listener {
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
     public static void onItemMove(InventoryMoveItemEvent event) {
+        if (event.getDestination().getType() == InventoryType.HOPPER) {
+            return;
+        }
+
         if (event.getSource() == null || event.getDestination().getHolder() instanceof BlockState) {
             return;
         }
